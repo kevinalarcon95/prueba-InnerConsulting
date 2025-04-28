430b80733335ceaefd384c6beb35d4a3780426be
Kevin Alarcón <57530393+kevinalarcon95@users.noreply.github.com>
2025-04-27 22:10:25 -0500

    feat: implement footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="bg-neutral-600/90 text-white p-4 text-center">
+      <p>&copy; 2025 innerConsulting. Todos los derechos reservados.</p>
+    </footer>
   )
 }
 
