75e2c54c05ab67ae4e737e6be870f923130a1b51
Kevin Alarcón <57530393+kevinalarcon95@users.noreply.github.com>
2025-04-27 22:06:28 -0500

    feat: update FormData type and enhance IInscriptionResponse interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,9 @@
 import { inscriptionSchema } from "@/components/forms/inscription-schema";
 import { z } from "zod";
 
-export type IFormData = z.infer<typeof inscriptionSchema>;
+export type FormDataType = z.infer<typeof inscriptionSchema>;
+
+export interface IInscriptionResponse {
+    id: number | string;
+    data: FormDataType;
+    }
